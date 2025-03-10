96d437b050f872557d5d60296bea8427b4361bf2
HamzaWebPro <hamza.mern2201@gmail.com>
2025-03-11 05:03:17 +0600

    added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./**/*.liquid", // Add this to scan Liquid files
+      "./**/*.html",
+      "./**/*.js",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
